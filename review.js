b379edda3a838eb5e31f255bf1aa830a5429e81d
Megha <66259297+meghagupta19@users.noreply.github.com>
2020-09-03 16:19:43 +0530

    Update review.js

--- review.js
+++ review.js
@@ -1,9 +1,13 @@
 const reviews = [
     {
         id: 1,
+        
         name: "Shancho Subrethann",
+        
         job:  "Web-developer",
+        
         img:  "https://images.pexels.com/photos/53487/james-stewart-man-person-actor-53487.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        
         text: "I had an amazing experience with this company.It provided me a lot of opportunities.\n\
         I started from basics and continued with many projects which improved my skills.Hereby \n\
         I also state that this company has a big role in making my career as we all need an\n\
@@ -13,9 +17,13 @@ const reviews = [
     },
     {
         id: 2,
+        
         name: "Rancho Mahajan",
+        
         job: "Software Engineer",
+        
         img: "https://images.pexels.com/photos/76961/audrey-hepburn-actress-breakfast-at-tiffany-s-prominent-76961.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        
         text: "I had an amazing experience with this company.\n\
         I started from basics and continued with many projects which improved my skills.\n\
         we allways need some opportunities and a platform to expell out our inner skills.We need an\n\
@@ -25,8 +33,11 @@ const reviews = [
     },
     {
         id: 3,
+        
         name: "Pontevar Scheenjanm",
+        
         job:  "App Developer",
+        
         img:  "https://images.pexels.com/photos/3823493/pexels-photo-3823493.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         text: "I am glad for joining this company.It provided me a lot of opportunities.\n\
                Doing projects and going through various exercises made me able to compete with \n\
@@ -34,9 +45,13 @@ const reviews = [
     },
     {
         id: 4,
+        
         name: "Jack Hangothon",
+        
         job:  "Web Developer",
+        
         img:  "https://images.pexels.com/photos/4865526/pexels-photo-4865526.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        
         text: "You don't own experiences rather you have to build some.This company\n\
                provided me a lot of opportunities.I started my career from just basics and now here\n\
                I am!!.Learning doesn't need age but focus and some willingness to create."   
@@ -55,32 +70,41 @@ const nextBtn = document.querySelector(".next-btn");
 let currentItem = 0;
 
 window.addEventListener("DOMContentLoaded",function(){
+    
     showReviews();
    
 });
 
 function showReviews(){
+    
     const item = reviews[currentItem];
     image.src = item.img;
     author.textContent = item.name;
     profession.textContent = item.job;
     information.textContent = item.text;
+    
 }
 
 nextBtn.addEventListener("click",function(){
+    
     currentItem++;
+    
     if(currentItem>reviews.length-1){
         currentItem = 0;
     }
     showReviews(currentItem);
+    
 });
 
 prevBtn.addEventListener("click",function(){
+    
     currentItem--;
+    
     if(currentItem < 0){
         currentItem = reviews.length-1;
     }
     showReviews(currentItem);
+    
 });
 
 
